cc53cff4c8fa87771bb87176e3b843b56f0ddaff
Ivette Landaverde <if722626@iteso.mx>
2022-06-18 14:30:13 -0500

    funciones

--- functions.py
+++ functions.py
@@ -78,6 +78,11 @@ minutes = list(np.arange(0,60))# esto es lo que queremos valuar, del 1 al 59
 
 
 
+
+
+
+
+
 #Pendientes:
 #1: hacerlo por cada minuto
 #2: todo lo anterior tmb para weighted midprice
@@ -88,3 +93,101 @@ minutes = list(np.arange(0,60))# esto es lo que queremos valuar, del 1 al 59
 # --------- ROLL MODEL ------- #
 
 # --------- ROLL MODEL ------- #
+
+
+
+
+
+
+
+# -- read input data:
+data_ob=dt.ob_data
+
+# ------ hacer el experimento 1 en función  -----
+def experimento_1_midprice(data_ob):
+    n_libros = len(list(data_ob.keys()))
+    #print(f"La cantidad total de libros de ordenes es: {n_libros}")
+
+    # -- calcular el midprice:
+    ob_ts=list(data_ob.keys())
+    l_ts= [pd.to_datetime(i_ts) for i_ts in ob_ts]
+    mid_price = [(data_ob[ob_ts[i]]['ask'][0]+data_ob[ob_ts[i]]['bid'][0])*0.5 for i in range(0,len(ob_ts))]
+    # guardar el midprice para contabilizarlo con P_t = E[P_t +1]
+    # guardar info en un diccionario
+    total = len(mid_price)-1
+    e1 = [mid_price[i] == mid_price[i+1] for i in range(len(mid_price)-1)]
+    e2 = len(mid_price) -1 - sum(e1)
+    exp_1 = {'e1':{'cantidad': sum(e1), 'proporcion': np.round(sum(e1)/total,2)},'e2':{'cantidad': e2, 'proporcion': np.round(e2/total,2)},'total': len(mid_price)-1}
+    df_exp_1 = pd.DataFrame({'e1':{'cantidad': sum(e1), 'proporcion': np.round(sum(e1)/total,2)},'e2':{'cantidad': e2, 'proporcion': np.round(e2/total,2)},'total': len(mid_price)-1})
+    # comprobar que tiene un comportamiento martingala:
+    # imprimir los resultados:
+    exp_1['e1']['proporcion']
+    exp_1['e2']['proporcion']
+    return df_exp_1
+
+# ------ hacer el experimento 2 en función  -----
+#primero crear un dataframe con el midprice:
+df2 = pd.DataFrame()
+df2['Timestamp'] = None
+df2 = df2.assign(Midprices=None)
+times = (l_ts)
+mids = [(data_ob[ob_ts[i]]['ask'][0]+data_ob[ob_ts[i]]['bid'][0])*0.5 for i in range(0,len(ob_ts))]
+df2['Timestamp'] = times
+df2['Midprices'] = mids
+df2 = df2.set_index('Timestamp')
+#...
+dic = {}
+for index, row in df2.iterrows():
+    key = str(index.hour) + ":" + str(index.minute)
+    value = row["Midprices"]
+    try:
+        dic[key].append(value)
+    except KeyError:
+        dic[key] = [value]
+    
+metricasMin = {}
+for i in list(dic): 
+    e1 = sum([dic[i][i_t] == dic[i][i_t+1] for i_t in range(len(dic[i])-1)])
+    e2 = len(dic[i])-e1
+    metricasMin[i] = (
+        {"e1" :{"cantidad" :  e1, "proporcion" : e1/len(dic[i])}, 
+         "e2" :{"cantidad" :  e2, "proporcion" : e2/len(dic[i])},
+         "total" : len(dic[i])
+         }
+        )  
+tot = []
+for i in list(metricasMin):
+    tot.append(metricasMin[i]["total"])
+np.array(tot).sum()
+# Proporción de frecuencia de martingalas
+mgE1 = []
+for i in list(metricasMin):
+    mgE1.append(metricasMin[i]["e1"]["cantidad"])
+np.array(mgE1).sum()/np.array(tot).sum()
+# Promedio de proporción de martingalas
+propE1 = []
+for i in list(metricasMin):
+    propE1.append(metricasMin[i]["e1"]["proporcion"])
+np.array(propE1).mean()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def experimento_2_midprice(data_ob):
+    return exp_2
+
+# ------- hacer el experimento 1 en funcion con el weighted midprice 
+
+# ------- hacer el experimento 2 en funcion con el weighted midprice 
